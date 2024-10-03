ec475d3f6853fc65176c37d8d936f08838bc564a
Apu-Mirza <mirzaa012323@gmail.com>
2024-10-03 23:40:56 +0600

    react project updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+
 import ReactDOM from 'react-dom';
+import React from "react";
 
 const index = 0;
 
-setInterval(() => {
-  const element = (
-    <h1 className="heading" tabIndex={index}>
-      <span className="text">Hello World {new Date().toLocaleTimeString()}</span>
-      <img src="" alt="" />
-    </h1>
-  )
-  ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+class Clock extends React.Component{
+
+  render(){
+
+      return (
+      <h1 className="heading" tabIndex={index}>
+        <span className="text">Hello World {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
+      </h1>
+    )
+  }
+  
+}
+  ReactDOM.render(<Clock locale="bn-BD">test</Clock>, document.getElementById('root'));
 
